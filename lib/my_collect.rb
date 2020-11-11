24bbf8f63e04ee8e69d3e609334ce98eebeb5ba5
Sean <skabe89@gmail.com>
2020-11-11 13:54:20 -0500

    commit tree 291286cbe078f66ff63b0b1b2781afc1d6e97b5c

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+require 'pry'
 
+def my_collect(array)
+    
+    if block_given?
+        i = 0
+        place = []
+        while i < array.length
+            place << yield(array[i])
+            i += 1
+        end
+        place
+    else
+       "Enter an array" 
+    end
+end
+
+#binding.pry
 
